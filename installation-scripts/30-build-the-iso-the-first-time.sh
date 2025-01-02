e210206cbaeb6904f80dcd28ca7ec12343a36d14
Pedro Gutierrez <pedrog4747@gmail.com>
2025-01-01 23:53:03 -0300

    Remove stuff i dont need

--- installation-scripts/30-build-the-iso-the-first-time.sh
+++ installation-scripts/30-build-the-iso-the-first-time.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #set -e
-#tput setaf 0 = black 
-#tput setaf 1 = red 
+#tput setaf 0 = black
+#tput setaf 1 = red
 #tput setaf 2 = green
-#tput setaf 3 = yellow 
-#tput setaf 4 = dark blue 
+#tput setaf 3 = yellow
+#tput setaf 4 = dark blue
 #tput setaf 5 = purple
-#tput setaf 6 = cyan 
-#tput setaf 7 = gray 
+#tput setaf 6 = cyan
+#tput setaf 7 = gray
 #tput setaf 8 = light blue
 ##################################################################################################################
 # Author	:	Erik Dubois
@@ -24,17 +24,17 @@
 #
 ##################################################################################################################
 
-if 	lsblk -f | grep btrfs > /dev/null 2>&1 ; then
-	echo
-	echo "################################################################## "
-	tput setaf 3
-	echo "Message"
-    echo "This script has been known to cause issues on a Btrfs filesystem"
-    echo "Make backups before continuing"
-    echo "Continu at your own risk"
-    tput sgr0
-    echo
-    read -p "Press Enter to continue... CTRL + C to stop"
+if lsblk -f | grep btrfs >/dev/null 2>&1; then
+  echo
+  echo "################################################################## "
+  tput setaf 3
+  echo "Message"
+  echo "This script has been known to cause issues on a Btrfs filesystem"
+  echo "Make backups before continuing"
+  echo "Continu at your own risk"
+  tput sgr0
+  echo
+  read -p "Press Enter to continue... CTRL + C to stop"
 fi
 
 echo
@@ -57,61 +57,59 @@ tput sgr0
 echo "################################################################## "
 echo
 
-	#Let us set the desktop"
-	#First letter of desktop is small letter
+#Let us set the desktop"
+#First letter of desktop is small letter
 
-	desktop="plasma"
-	dmDesktop="plasma"
+desktop="plasma"
+dmDesktop="plasma"
 
-	arcolinuxVersion='v25.02.02'
+arcolinuxVersion='v25.02.02'
 
-	isoLabel='arcoplasma-'$arcolinuxVersion'-x86_64.iso'
+isoLabel='arcoplasma-'$arcolinuxVersion'-x86_64.iso'
 
-	# setting of the general parameters
-	archisoRequiredVersion="archiso 82-1"
-	buildFolder=$HOME"/arcoplasma-build"
-	outFolder=$HOME"/arcoplasma-Out"
-	archisoVersion=$(sudo pacman -Q archiso)
+# setting of the general parameters
+archisoRequiredVersion="archiso 82-1"
+buildFolder=$HOME"/arcoplasma-build"
+outFolder=$HOME"/arcoplasma-Out"
+archisoVersion=$(sudo pacman -Q archiso)
 
-	# If you want to add packages from the chaotics-aur repo then
-	# change the variable to true and add the package names
-	# that are hosted on chaotics-aur in the packages.x86_64 at the bottom
+# If you want to add packages from the chaotics-aur repo then
+# change the variable to true and add the package names
+# that are hosted on chaotics-aur in the packages.x86_64 at the bottom
 
-	chaoticsrepo=false
-	
-	# If you are ready to use your personal repo and personal packages
-	# https://arcolinux.com/use-our-knowledge-and-create-your-own-icon-theme-combo-use-github-to-saveguard-your-work/
-	# 1. set variable personalrepo to true in this file (default:false)
-	# 2. change the file personal-repo to reflect your repo
-	# 3. add your applications to the file packages-personal-repo.x86_64
+chaoticsrepo=false
 
-	personalrepo=false
+# If you are ready to use your personal repo and personal packages
+# https://arcolinux.com/use-our-knowledge-and-create-your-own-icon-theme-combo-use-github-to-saveguard-your-work/
+# 1. set variable personalrepo to true in this file (default:false)
+# 2. change the file personal-repo to reflect your repo
+# 3. add your applications to the file packages-personal-repo.x86_64
 
-	echo "################################################################## "
-	echo "Building the desktop                   : "$desktop
-	echo "Building version                       : "$arcolinuxVersion
-	echo "Iso label                              : "$isoLabel
-	echo "Do you have the right archiso version? : "$archisoVersion
-	echo "What is the required archiso version?  : "$archisoRequiredVersion
-	echo "Build folder                           : "$buildFolder
-	echo "Out folder                             : "$outFolder
-	echo "################################################################## "
+echo "################################################################## "
+echo "Building the desktop                   : "$desktop
+echo "Building version                       : "$arcolinuxVersion
+echo "Iso label                              : "$isoLabel
+echo "Do you have the right archiso version? : "$archisoVersion
+echo "What is the required archiso version?  : "$archisoRequiredVersion
+echo "Build folder                           : "$buildFolder
+echo "Out folder                             : "$outFolder
+echo "################################################################## "
 
-	if [ "$archisoVersion" == "$archisoRequiredVersion" ]; then
-		tput setaf 2
-		echo "##################################################################"
-		echo "Archiso has the correct version. Continuing ..."
-		echo "##################################################################"
-		tput sgr0
-	else
-	tput setaf 1
-	echo "###################################################################################################"
-	echo "You need to install the correct version of Archiso"
-	echo "Use 'sudo downgrade archiso' to do that"
-	echo "or update your system"
-	echo "###################################################################################################"
-	tput sgr0
-	fi
+if [ "$archisoVersion" == "$archisoRequiredVersion" ]; then
+  tput setaf 2
+  echo "##################################################################"
+  echo "Archiso has the correct version. Continuing ..."
+  echo "##################################################################"
+  tput sgr0
+else
+  tput setaf 1
+  echo "###################################################################################################"
+  echo "You need to install the correct version of Archiso"
+  echo "Use 'sudo downgrade archiso' to do that"
+  echo "or update your system"
+  echo "###################################################################################################"
+  tput sgr0
+fi
 
 echo
 echo "################################################################## "
@@ -124,57 +122,57 @@ tput sgr0
 echo "################################################################## "
 echo
 
-	package="archiso"
+package="archiso"
 
-	#----------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------
 
-	#checking if application is already installed or else install with aur helpers
-	if pacman -Qi $package &> /dev/null; then
+#checking if application is already installed or else install with aur helpers
+if pacman -Qi $package &>/dev/null; then
 
-			echo "Archiso is already installed"
+  echo "Archiso is already installed"
 
-	else
+else
 
-		#checking which helper is installed
-		if pacman -Qi yay &> /dev/null; then
+  #checking which helper is installed
+  if pacman -Qi yay &>/dev/null; then
 
-			echo "################################################################"
-			echo "######### Installing with yay"
-			echo "################################################################"
-			yay -S --noconfirm $package
+    echo "################################################################"
+    echo "######### Installing with yay"
+    echo "################################################################"
+    yay -S --noconfirm $package
 
-		elif pacman -Qi trizen &> /dev/null; then
+  elif pacman -Qi trizen &>/dev/null; then
 
-			echo "################################################################"
-			echo "######### Installing with trizen"
-			echo "################################################################"
-			trizen -S --noconfirm --needed --noedit $package
+    echo "################################################################"
+    echo "######### Installing with trizen"
+    echo "################################################################"
+    trizen -S --noconfirm --needed --noedit $package
 
-		fi
+  fi
 
-		# Just checking if installation was successful
-		if pacman -Qi $package &> /dev/null; then
+  # Just checking if installation was successful
+  if pacman -Qi $package &>/dev/null; then
 
-			echo "################################################################"
-			echo "#########  "$package" has been installed"
-			echo "################################################################"
+    echo "################################################################"
+    echo "#########  "$package" has been installed"
+    echo "################################################################"
 
-		else
+  else
 
-			echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-			echo "!!!!!!!!!  "$package" has NOT been installed"
-			echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-			exit 1
-		fi
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    echo "!!!!!!!!!  "$package" has NOT been installed"
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    exit 1
+  fi
 
-	fi
+fi
 
-	echo
-	echo "Saving current archiso version to readme"
-	sudo sed -i "s/\(^archiso-version=\).*/\1$archisoVersion/" ../archiso.readme
-	echo
-	echo "Making mkarchiso verbose"
-	sudo sed -i 's/quiet="y"/quiet="n"/g' /usr/bin/mkarchiso
+echo
+echo "Saving current archiso version to readme"
+sudo sed -i "s/\(^archiso-version=\).*/\1$archisoVersion/" ../archiso.readme
+echo
+echo "Making mkarchiso verbose"
+sudo sed -i 's/quiet="y"/quiet="n"/g' /usr/bin/mkarchiso
 
 echo
 echo "################################################################## "
@@ -186,13 +184,13 @@ tput sgr0
 echo "################################################################## "
 echo
 
-	echo "Deleting the build folder if one exists - takes some time"
-	[ -d $buildFolder ] && sudo rm -rf $buildFolder
-	echo
-	echo "Copying the Archiso folder to build work"
-	echo
-	mkdir $buildFolder
-	cp -r ../archiso $buildFolder/archiso
+echo "Deleting the build folder if one exists - takes some time"
+[ -d $buildFolder ] && sudo rm -rf $buildFolder
+echo
+echo "Copying the Archiso folder to build work"
+echo
+mkdir $buildFolder
+cp -r ../archiso $buildFolder/archiso
 
 echo
 echo "################################################################## "
@@ -207,64 +205,49 @@ tput sgr0
 echo "################################################################## "
 echo
 
-	echo "Deleting any files in /etc/skel"
-	rm -rf $buildFolder/archiso/airootfs/etc/skel/.* 2> /dev/null
-	echo
+echo "Deleting any files in /etc/skel"
+rm -rf $buildFolder/archiso/airootfs/etc/skel/.* 2>/dev/null
+echo
 
-	echo "Getting the last version of bashrc in /etc/skel"
-	echo
-	wget https://raw.githubusercontent.com/arcolinux/arcolinux-root/master/etc/skel/.bashrc-latest -O $buildFolder/archiso/airootfs/etc/skel/.bashrc
+echo "Getting the last version of bashrc in /etc/skel"
+echo
+wget https://raw.githubusercontent.com/arcolinux/arcolinux-root/master/etc/skel/.bashrc-latest -O $buildFolder/archiso/airootfs/etc/skel/.bashrc
 
-	echo "Removing the old packages.x86_64 file from build folder"
-	rm $buildFolder/archiso/packages.x86_64
-	rm $buildFolder/archiso/packages-personal-repo.x86_64
-	echo
+echo "Removing the old packages.x86_64 file from build folder"
+rm $buildFolder/archiso/packages.x86_64
+echo
 
-	echo "Copying the new packages.x86_64 file to the build folder"
-	cp -f ../archiso/packages.x86_64 $buildFolder/archiso/packages.x86_64
-	echo
+echo "Copying the new packages.x86_64 file to the build folder"
+cp -f ../archiso/packages.x86_64 $buildFolder/archiso/packages.x86_64
+echo
 
-	if [ $personalrepo == true ]; then
-		echo "Adding packages from your personal repository - packages-personal-repo.x86_64"
-		printf "\n" | sudo tee -a $buildFolder/archiso/packages.x86_64
-		cat ../archiso/packages-personal-repo.x86_64 | sudo tee -a $buildFolder/archiso/packages.x86_64
-	fi
+if [ $chaoticsrepo == true ]; then
+  echo "Adding our chaotics repo to /etc/pacman.conf"
+  printf "\n" | sudo tee -a $buildFolder/archiso/pacman.conf
+  printf "\n" | sudo tee -a $buildFolder/archiso/airootfs/etc/pacman.conf
+  cat chaotics-repo | sudo tee -a $buildFolder/archiso/pacman.conf
+  cat chaotics-repo | sudo tee -a $buildFolder/archiso/airootfs/etc/pacman.conf
+fi
 
-	if [ $personalrepo == true ]; then
-		echo "Adding our own repo to /etc/pacman.conf"
-		printf "\n" | sudo tee -a $buildFolder/archiso/pacman.conf
-		printf "\n" | sudo tee -a $buildFolder/archiso/airootfs/etc/pacman.conf
-		cat personal-repo | sudo tee -a $buildFolder/archiso/pacman.conf
-		cat personal-repo | sudo tee -a $buildFolder/archiso/airootfs/etc/pacman.conf
-	fi
+if [ $chaoticsrepo == false ]; then
+  echo "Adding our chaotics repo to /etc/pacman.conf"
+  printf "\n" | sudo tee -a $buildFolder/archiso/pacman.conf
+  printf "\n" | sudo tee -a $buildFolder/archiso/airootfs/etc/pacman.conf
+  cat no-chaotics-repo | sudo tee -a $buildFolder/archiso/pacman.conf
+  cat no-chaotics-repo | sudo tee -a $buildFolder/archiso/airootfs/etc/pacman.conf
+fi
 
-	if [ $chaoticsrepo == true ]; then
-		echo "Adding our chaotics repo to /etc/pacman.conf"
-		printf "\n" | sudo tee -a $buildFolder/archiso/pacman.conf
-		printf "\n" | sudo tee -a $buildFolder/archiso/airootfs/etc/pacman.conf
-		cat chaotics-repo | sudo tee -a $buildFolder/archiso/pacman.conf
-		cat chaotics-repo | sudo tee -a $buildFolder/archiso/airootfs/etc/pacman.conf
-	fi
+echo
+echo "Adding the content of the /personal folder"
+echo
+cp -rf ../personal/ $buildFolder/archiso/airootfs/
 
-	if [ $chaoticsrepo == false ]; then
-		echo "Adding our chaotics repo to /etc/pacman.conf"
-		printf "\n" | sudo tee -a $buildFolder/archiso/pacman.conf
-		printf "\n" | sudo tee -a $buildFolder/archiso/airootfs/etc/pacman.conf
-		cat no-chaotics-repo | sudo tee -a $buildFolder/archiso/pacman.conf
-		cat no-chaotics-repo | sudo tee -a $buildFolder/archiso/airootfs/etc/pacman.conf
-	fi
-
-	echo
-	echo "Adding the content of the /personal folder"
-	echo
-	cp -rf ../personal/ $buildFolder/archiso/airootfs/
-
-	if test -f $buildFolder/archiso/airootfs/personal/.gitkeep ; then
-		echo
-		rm $buildFolder/archiso/airootfs/personal/.gitkeep
-		echo ".gitkeep is now removed"
-		echo
-    fi
+if test -f $buildFolder/archiso/airootfs/personal/.gitkeep; then
+  echo
+  rm $buildFolder/archiso/airootfs/personal/.gitkeep
+  echo ".gitkeep is now removed"
+  echo
+fi
 
 echo
 echo "################################################################## "
@@ -276,39 +259,37 @@ tput sgr0
 echo "################################################################## "
 echo
 
-	#Setting variables
+#Setting variables
 
-	#profiledef.sh
-	oldname1='iso_name="arcoplasma'
-	newname1='iso_name="arcoplasma'
+#profiledef.sh
+oldname1='iso_name="arcoplasma'
+newname1='iso_name="arcoplasma'
 
-	oldname2='iso_label="arcoplasma'
-	newname2='iso_label="arcoplasma'
+oldname2='iso_label="arcoplasma'
+newname2='iso_label="arcoplasma'
 
-	oldname3='arcoplasma'
-	newname3='arcoplasma'
+oldname3='arcoplasma'
+newname3='arcoplasma'
 
-	#hostname
-	oldname4='arcoplasma'
-	newname4='arcoplasma'
+#hostname
+oldname4='arcoplasma'
+newname4='arcoplasma'
 
-	#sddm.conf user-session
-	oldname5='Session=xfce'
-	newname5='Session='$dmDesktop
+#sddm.conf user-session
+oldname5='Session=xfce'
+newname5='Session='$dmDesktop
 
-	echo "Changing all references"
-	echo
-	sed -i 's/'$oldname1'/'$newname1'/g' $buildFolder/archiso/profiledef.sh
-	sed -i 's/'$oldname2'/'$newname2'/g' $buildFolder/archiso/profiledef.sh
-	sed -i 's/'$oldname3'/'$newname3'/g' $buildFolder/archiso/airootfs/etc/dev-rel
-	sed -i 's/'$oldname4'/'$newname4'/g' $buildFolder/archiso/airootfs/etc/hostname
-	sed -i 's/'$oldname5'/'$newname5'/g' $buildFolder/archiso/airootfs/etc/sddm.conf.d/kde_settings.conf
+echo "Changing all references"
+echo
+sed -i 's/'$oldname1'/'$newname1'/g' $buildFolder/archiso/profiledef.sh
+sed -i 's/'$oldname2'/'$newname2'/g' $buildFolder/archiso/profiledef.sh
+sed -i 's/'$oldname3'/'$newname3'/g' $buildFolder/archiso/airootfs/etc/dev-rel
+sed -i 's/'$oldname4'/'$newname4'/g' $buildFolder/archiso/airootfs/etc/hostname
 
-	echo "Adding time to /etc/dev-rel"
-	date_build=$(date -d now)
-	echo "Iso build on : "$date_build
-	sudo sed -i "s/\(^ISO_BUILD=\).*/\1$date_build/" $buildFolder/archiso/airootfs/etc/dev-rel
-
+echo "Adding time to /etc/dev-rel"
+date_build=$(date -d now)
+echo "Iso build on : "$date_build
+sudo sed -i "s/\(^ISO_BUILD=\).*/\1$date_build/" $buildFolder/archiso/airootfs/etc/dev-rel
 
 echo
 echo "###########################################################"
@@ -319,8 +300,8 @@ tput sgr0
 echo "###########################################################"
 echo
 
-	echo "Cleaning the cache from /var/cache/pacman/pkg/"
-	yes | sudo pacman -Scc
+echo "Cleaning the cache from /var/cache/pacman/pkg/"
+yes | sudo pacman -Scc
 
 echo
 echo "################################################################## "
@@ -331,11 +312,9 @@ tput sgr0
 echo "################################################################## "
 echo
 
-	[ -d $outFolder ] || mkdir $outFolder
-	cd $buildFolder/archiso/
-	sudo mkarchiso -v -w $buildFolder -o $outFolder $buildFolder/archiso/
-
-
+[ -d $outFolder ] || mkdir $outFolder
+cd $buildFolder/archiso/
+sudo mkarchiso -v -w $buildFolder -o $outFolder $buildFolder/archiso/
 
 echo
 echo "###################################################################"
@@ -347,25 +326,25 @@ tput sgr0
 echo "###################################################################"
 echo
 
-	cd $outFolder
+cd $outFolder
 
-	echo "Creating checksums for : "$isoLabel
-	echo "##################################################################"
-	echo
-	echo "Building sha1sum"
-	echo "########################"
-	sha1sum $isoLabel | tee $isoLabel.sha1
-	echo "Building sha256sum"
-	echo "########################"
-	sha256sum $isoLabel | tee $isoLabel.sha256
-	echo "Building md5sum"
-	echo "########################"
-	md5sum $isoLabel | tee $isoLabel.md5
-	echo
-	echo "Moving pkglist.x86_64.txt"
-	echo "########################"
-	cp $buildFolder/iso/arch/pkglist.x86_64.txt  $outFolder/$isoLabel".pkglist.txt"
-	
+echo "Creating checksums for : "$isoLabel
+echo "##################################################################"
+echo
+echo "Building sha1sum"
+echo "########################"
+sha1sum $isoLabel | tee $isoLabel.sha1
+echo "Building sha256sum"
+echo "########################"
+sha256sum $isoLabel | tee $isoLabel.sha256
+echo "Building md5sum"
+echo "########################"
+md5sum $isoLabel | tee $isoLabel.md5
+echo
+echo "Moving pkglist.x86_64.txt"
+echo "########################"
+cp $buildFolder/iso/arch/pkglist.x86_64.txt $outFolder/$isoLabel".pkglist.txt"
+
 echo
 echo "##################################################################"
 tput setaf 2
@@ -375,8 +354,8 @@ tput sgr0
 echo "################################################################## "
 echo
 
-	echo "Deleting the build folder if one exists - takes some time"
-	[ -d $buildFolder ] && sudo rm -rf $buildFolder
+echo "Deleting the build folder if one exists - takes some time"
+[ -d $buildFolder ] && sudo rm -rf $buildFolder
 
 echo
 echo "##################################################################"
